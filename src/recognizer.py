50204a6d5985492d408073a880efc932e42ad141
Kuznetsov Evgeniy <kuzevgleo@gmail.com>
2021-03-16 17:34:27 +0300

    GODR-11: add edges recognition, improve lines recognition

--- src/recognizer.py
+++ src/recognizer.py
@@ -11,10 +11,11 @@ class Recognizer:
         img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         v_lines, h_lines = self.lines_recognition(img_gray)
         intersections = self.find_intersections(v_lines, h_lines)
+        edges = self.find_edges(v_lines, h_lines)
         cell_size = self.get_cell_size(v_lines, h_lines)
         stones, radius = self.stones_recognition(img_gray, cell_size, intersections)
         white_stones, black_stones = self.colorize(img_gray, stones, radius)
-        return intersections, cell_size, white_stones, black_stones, radius
+        return intersections, cell_size, white_stones, black_stones, radius, edges
         
     def to_RGB(self, image):
         return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -25,8 +26,8 @@ class Recognizer:
         return cv2.Canny(img_gray, CANNY_THRESHOLD1, CANNY_THRESHOLD2)
     
     def all_lines(self, img_gray, hough_threshold):
-        HOUGH_RHO = 1
-        HOUGH_THETA = np.pi / 180
+        #HOUGH_RHO = 1
+        #HOUGH_THETA = np.pi / 180
 
         edges = self.get_edges(img_gray)
         size = min(img_gray.shape[0:2])
@@ -49,25 +50,39 @@ class Recognizer:
         h_lines = []
         for line in lines:
             x1, y1, x2, y2 = line
-            if (x1 == x2):
+            if x1 == x2:
+                if (y1 > y2):
+                    line[0] = x2
+                    line[1] = y2
+                    line[2] = x1
+                    line[3] = y1
                 v_lines.append(line)
             else:
                 slope = (y2 - y1) / (x2 - x1)
-                if (abs(slope) > VERTICAL_TAN_MIN):
-                    v_lines.append(line)
-                elif (abs(slope) < HORIZONTAL_TAN_MAX):
+                if abs(slope) > VERTICAL_TAN_MIN:
+                    if (y1 > y2):
+                        line[0] = x2
+                        line[1] = y2
+                        line[2] = x1
+                        line[3] = y1
+                elif abs(slope) < HORIZONTAL_TAN_MAX:
+                    if (x1 > x2):
+                        line[0] = x2
+                        line[1] = y2
+                        line[2] = x1
+                        line[3] = y1
                     h_lines.append(line)
         v_lines = np.array(v_lines)
         h_lines = np.array(h_lines)
         # Merge close lines
         return self.merge_lines(v_lines, True), self.merge_lines(h_lines, False)
-    
+
     def merge_lines(self, lines, is_vertical):
         # ???
         #MIN_DIST = 10
 
         close_groups = []
-        grouped = set() 
+        grouped = set()
         for i in range(lines.shape[0]):
             if i in grouped:
                 continue
@@ -86,20 +101,19 @@ class Recognizer:
         merged_lines = []
         for group in close_groups:
             line_group = lines[group]
-            # possible bag
             if is_vertical:
                 x = round(np.mean(line_group[:, [0, 2]]))
-                merged_line = (x, np.amax(line_group[:, 1]), x, np.amin(line_group[:, 3]))
+                merged_line = (x, np.amin(line_group[:, 1]), x, np.amax(line_group[:, 3]))
             else:
                 y = round(np.mean(line_group[:, [1, 3]]))
-                merged_line = (np.amin(line_group[:, 0]), y, np.max(line_group[:, 2]), y)
+                merged_line = (np.amin(line_group[:, 0]), y, np.amax(line_group[:, 2]), y)
             merged_lines.append(merged_line)
 
         if is_vertical:
-            return np.array(sorted(merged_lines, key=lambda line:line[0]))
+            return np.array(sorted(merged_lines, key=lambda line: line[0]))
         else:
-            return np.array(sorted(merged_lines, key=lambda line:line[1]))
-    
+            return np.array(sorted(merged_lines, key=lambda line: line[1]))
+
     def lines_recognition(self, img_gray):
         #HOUGH_LOW_THRESHOLD = 50
         #HOUGH_HIGH_THRESHOLD = 200
@@ -119,47 +133,67 @@ class Recognizer:
         ind = bisect(unclear_v_lines[:, 0], clear_v_lines[0][0])
         for i in range(ind - 1, -1, -1):
             dist_coeff = (v_lines[-1][0] - unclear_v_lines[i][0]) / cell_size
-            if dist_coeff > MIN_LINES_DIST_COEFF:
-                if dist_coeff > MIN_GAP_COEFF:
-                    x = (unclear_v_lines[i][0] + v_lines[-1][0]) // 2
-                    new_v_line = [x, v_lines[-1][1], x, v_lines[-1][3]]
-                    v_lines.append(new_v_line)
+            if MAX_LINES_DIST_COEFF > dist_coeff > MIN_LINES_DIST_COEFF:
+                v_lines.append(unclear_v_lines[i])
+            if dist_coeff > MIN_GAP_COEFF:
+                x = (unclear_v_lines[i][0] + v_lines[-1][0]) // 2
+                new_v_line = [x, v_lines[-1][1], x, v_lines[-1][3]]
+                v_lines.append(new_v_line)
                 v_lines.append(unclear_v_lines[i])
         v_lines.reverse()
         for i in range(ind, len(unclear_v_lines)):
             dist_coeff = (unclear_v_lines[i][0] - v_lines[-1][0]) / cell_size
-            if dist_coeff > MIN_LINES_DIST_COEFF:
-                if dist_coeff > MIN_GAP_COEFF:
-                    x = (unclear_v_lines[i][0] + v_lines[-1][0]) // 2
-                    new_v_line = [x, v_lines[-1][1], x, v_lines[-1][3]]
-                    v_lines.append(new_v_line)
+            if MAX_LINES_DIST_COEFF > dist_coeff > MIN_LINES_DIST_COEFF:
+                v_lines.append(unclear_v_lines[i])
+            if dist_coeff > MIN_GAP_COEFF:
+                x = (unclear_v_lines[i][0] + v_lines[-1][0]) // 2
+                new_v_line = [x, v_lines[-1][1], x, v_lines[-1][3]]
+                v_lines.append(new_v_line)
                 v_lines.append(unclear_v_lines[i])
         ind = bisect(unclear_h_lines[:, 1], clear_h_lines[0][1])
         for i in range(ind - 1, -1, -1):
             dist_coeff = (h_lines[-1][1] - unclear_h_lines[i][1]) / cell_size
-            if dist_coeff > MIN_LINES_DIST_COEFF:
-                if dist_coeff > MIN_GAP_COEFF:
-                    y = (unclear_h_lines[i][1] + h_lines[-1][1]) // 2
-                    new_h_line = [h_lines[-1][0], y, h_lines[-1][2], y]
-                    h_lines.append(new_h_line)
+            if MAX_LINES_DIST_COEFF > dist_coeff > MIN_LINES_DIST_COEFF:
+                h_lines.append(unclear_h_lines[i])
+            if dist_coeff > MIN_GAP_COEFF:
+                y = (unclear_h_lines[i][1] + h_lines[-1][1]) // 2
+                new_h_line = [h_lines[-1][0], y, h_lines[-1][2], y]
+                h_lines.append(new_h_line)
                 h_lines.append(unclear_h_lines[i])
         h_lines.reverse()
         for i in range(ind, len(unclear_h_lines)):
             dist_coeff = (unclear_h_lines[i][1] - h_lines[-1][1]) / cell_size
-            if dist_coeff > MIN_LINES_DIST_COEFF:
-                if dist_coeff > MIN_GAP_COEFF:
-                    y = (unclear_h_lines[i][1] + h_lines[-1][1]) // 2
-                    new_h_line = [h_lines[-1][0], y, h_lines[-1][2], y]
-                    h_lines.append(new_h_line)
+            if MAX_LINES_DIST_COEFF > dist_coeff > MIN_LINES_DIST_COEFF:
+                h_lines.append(unclear_h_lines[i])
+            if dist_coeff > MIN_GAP_COEFF:
+                y = (unclear_h_lines[i][1] + h_lines[-1][1]) // 2
+                new_h_line = [h_lines[-1][0], y, h_lines[-1][2], y]
+                h_lines.append(new_h_line)
                 h_lines.append(unclear_h_lines[i])
         return np.array(v_lines), np.array(h_lines)
     
     def get_cell_size(self, v_lines, h_lines):
-        return round(np.mean([round(np.ptp(v_lines[:, 0]) / v_lines.shape[0] - 1),
-                              round(np.ptp(h_lines[:, 1]) / h_lines.shape[0] - 1)]))
+        return round(np.mean([round(np.ptp(v_lines[:, 0]) / (v_lines.shape[0] - 1)),
+                              round(np.ptp(h_lines[:, 1]) / (h_lines.shape[0] - 1))]))
     
     def find_intersections(self, v_lines, h_lines):
-        return np.array(np.meshgrid(v_lines[:, 0], h_lines[:, 1])).T.reshape(-1, 2)
+        return np.array(np.meshgrid(v_lines[:, 0], h_lines[:, 1])).T
+
+    def find_edges(self, v_lines, h_lines):
+        MIN_DIFF = 5
+
+        up_line = h_lines[0]
+        up_edge = np.sum(up_line[1] - v_lines[:, 1] > MIN_DIFF) < (v_lines.shape[0] / 1.5)
+
+        down_line = h_lines[-1]
+        down_edge = np.sum(v_lines[:, 3] - down_line[1] > MIN_DIFF) < (v_lines.shape[0] / 1.5)
+
+        left_line = v_lines[0]
+        left_edge = np.sum(left_line[0] - h_lines[:, 0] > MIN_DIFF) < (h_lines.shape[0] / 1.5)
+
+        right_line = v_lines[-1]
+        right_edge = np.sum(h_lines[:, 2] - right_line[0] > MIN_DIFF) < (h_lines.shape[0] / 1.5)
+        return up_edge, down_edge, left_edge, right_edge
 
     def find_circles(self, img_gray, cell_size):
         #METHOD = cv2.HOUGH_GRADIENT
@@ -184,7 +218,7 @@ class Recognizer:
         radii = []
         for circle in circles:
             in_intersection = False
-            for intersection in intersections:
+            for intersection in np.reshape(intersections, (-1, 2)):
                 if (np.linalg.norm(circle[:2] - intersection) <= MIN_INTERSECTION_DIST_COEFF * cell_size):
                     in_intersection = True
                     break
