d1e7743139deeae8415b29967bbd2c1523d94568
Vladislav Vikhrov <Vikhrov.vs@phystech.edu>
2021-03-28 23:49:10 +0300

    GODR-22, GODR-10: visualization updated

--- src/visualizer.py
+++ src/visualizer.py
@@ -9,91 +9,51 @@ class Visualizer:
 
         self.game = sgf.Sgf_game.from_bytes(game_bytes)
 
-    def draw_board(self, img=None, fig=None, n_boards = 1, current_index = 1):
+    def draw_board(self, img=None, fig=None, n_boards=1, current_index=1):
         board_size = self.game.get_size()
-        # fig.patch.set_facecolor((1, 1, .8))
+        board_len = 4
+
         if img is not None:
             if fig is None:
-                fig = plt.figure(figsize=[16, 8])
+                fig = plt.figure(figsize=[2 * board_len, board_len])
 
-            ax1 = fig.add_subplot(1, 2, 2)
-
-            for x in range(board_size):
-                ax1.plot([x, x], [0, board_size - 1], 'k')
-            for y in range(board_size):
-                ax1.plot([0, board_size - 1], [y, y], 'k')
-
-            ax1.set_axis_off()
-
-            ax1.set_xlim(-1, board_size)
-            ax1.set_ylim(-1, board_size)
-
-            root = self.game.get_root()
-            pos = root.get_setup_stones()
-            white_stones = pos[1]
-            black_stones = pos[0]
-
-            markersize = 450 / (board_size + 3)
-            for stone in white_stones:
-                y = stone[0]
-                x = stone[1]
-                ax1.plot(x, y, 'o', markersize=markersize, markeredgecolor=(0, 0, 0), markerfacecolor='w',
-                         markeredgewidth=2)
-
-            for stone in black_stones:
-                y = stone[0]
-                x = stone[1]
-                edgecolor = 0
-                ax1.plot(x, y, 'o', markersize=markersize, markeredgecolor=(edgecolor, edgecolor, edgecolor),
-                         markerfacecolor='k',
-                         markeredgewidth=2)
-
-            ax2 = fig.add_subplot(1, 2, 1)
-            # ax.add_subplot(1, 2, 1)
-            ax2.imshow(img)
-            ax2.axis('off')
-
+            ax1 = fig.add_subplot(n_boards, 2, 2*current_index)
         else:
-            '''
-            source: https://stackoverflow.com/questions/24563513/drawing-a-go-board-with-matplotlib
-            some constants need to be changed
-            '''
             if fig is None:
-                fig = plt.figure(figsize=[8, 8])
-            # fig.patch.set_facecolor((1, 1, .8))
-            # print(n_boards, 1, current_index)
+                fig = plt.figure(figsize=[board_len, board_len])
             ax1 = fig.add_subplot(n_boards, 1, current_index)
 
-            # draw the grid
-            for x in range(board_size):
-                ax1.plot([x, x], [0, board_size - 1], 'k')
-            for y in range(board_size):
-                ax1.plot([0, board_size - 1], [y, y], 'k')
+        # fig.patch.set_facecolor((1, 1, .8))
+        
+        for x in range(board_size):
+            ax1.plot([x, x], [0, board_size - 1], 'k')
+        for y in range(board_size):
+            ax1.plot([0, board_size - 1], [y, y], 'k')
 
-            # scale the axis area to fill the whole figure
-            # ax1.set_position([0, 0, 1, 1])
+        ax1.set_axis_off()
 
-            # get rid of axes and everything (the figure background will show through)
-            ax1.set_axis_off()
+        ax1.set_xlim(-1, board_size)
+        ax1.set_ylim(-1, board_size)
 
-            # scale the plot area conveniently (the board is in 0,0..18,18)
-            ax1.set_xlim(-1, board_size)
-            ax1.set_ylim(-1, board_size)
+        root = self.game.get_root()
+        pos = root.get_setup_stones()
+        black_stones = pos[0]
+        white_stones = pos[1]
 
-            root = self.game.get_root()
-            pos = root.get_setup_stones()
-            white_stones = pos[1]
-            black_stones = pos[0]
+        marker_size = 175 / (board_size + 1)
+        for stone in white_stones:
+            y = stone[0]
+            x = stone[1]
+            ax1.plot(x, y, 'o', markersize=marker_size, markeredgecolor=(0, 0, 0), markerfacecolor='w',
+                     markeredgewidth=2)
 
-            markersize = 150 / (board_size + 1)
-            for stone in white_stones:
-                y = stone[0]
-                x = stone[1]
-                ax1.plot(x, y, 'o', markersize=markersize, markeredgecolor=(0, 0, 0), markerfacecolor='w',
-                        markeredgewidth=2)
+        for stone in black_stones:
+            y = stone[0]
+            x = stone[1]
+            ax1.plot(x, y, 'o', markersize=marker_size, markeredgecolor=(.5, .5, .5), markerfacecolor='k',
+                     markeredgewidth=2)
 
-            for stone in black_stones:
-                y = stone[0]
-                x = stone[1]
-                ax1.plot(x, y, 'o', markersize=markersize, markeredgecolor=(.5, .5, .5), markerfacecolor='k',
-                        markeredgewidth=2)
+        if img is not None:
+            ax2 = fig.add_subplot(n_boards, 2, 2*current_index - 1)
+            ax2.imshow(img)
+            ax2.axis('off')
